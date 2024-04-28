0bb9d1a1e3f96141d7ea84b9a67ae804a09a89fd
Maria Gregorio <airam.greg@gmail.com>
2024-04-28 20:08:46 -0300

    add more tests for throws

--- src/tests/Megaverse.test.ts
+++ src/tests/Megaverse.test.ts
@@ -2,29 +2,38 @@ import { DrawServiceMock } from './mocks';
 import { Megaverse } from '../Megaverse';
 
 describe('Megaverse', () => {
-    beforeAll(() => {
-        process.env.BATCH_DELAY = '1';
-        process.env.CANDIDATE_ID = '123';
-    });
+    process.env.CANDIDATE_ID = '123';
+    process.env.BATCH_DELAY = '1';
+    const drawService = new DrawServiceMock();
+    const megaverse = new Megaverse(drawService);
 
     afterEach(() => {
         jest.clearAllMocks();
     });
 
     test('should draw polyanets cross with expected number of requests', async () => {
-        const drawService = new DrawServiceMock();
         const spy = jest.spyOn(drawService, 'drawElement');
-        const megaverse = new Megaverse(drawService);
 
         await megaverse.drawPOLYanetCross(11, 11, 7);
 
         expect(spy).toHaveBeenCalledTimes(13);
     });
 
+    test('should throw when cross exceeds limit', async () => {
+        expect(async () => await megaverse.drawPOLYanetCross(5, 5, 7)).rejects.toThrow(
+            'Cross size exceeds megaverse size limit'
+        );
+    });
+
+    test('should throw when cross size is not odd', async () => {
+        expect(async () => await megaverse.drawPOLYanetCross(5, 5, 2)).rejects.toThrow(
+            'Cross size must be an odd number'
+        );
+    });
+
     test('should draw logo with expected number of requests', async () => {
-        const drawService = new DrawServiceMock();
         const spy = jest.spyOn(drawService, 'drawElement');
-        const megaverse = new Megaverse(drawService);
+
         const goal = await drawService.getGoal();
         const elementCount = goal.goal.length * goal.goal[0].length;
 
